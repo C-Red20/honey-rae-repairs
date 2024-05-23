d132420ec12a5e70452cda04bb16688cbf10952a
C-Red20 <caila.linger@yahoo.com>
2024-05-23 13:55:59 -0400

    Employee Add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css"
 import { CustomerList } from "./components/customers/CustomersList.jsx"
 import { TicketList } from "./components/tickets/TicketList.jsx"
+import { EmployeeList } from "./components/employees/EmployeeList.jsx"
+
 
 export const App = () => {
   return (
     <>
       {/* <TicketList /> */}
-      <CustomerList />
+      {/* <CustomerList /> */}
+      <EmployeeList />
     </>
   )
 }
