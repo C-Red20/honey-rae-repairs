7b4bae30f217594b1724e3ab3179b2af3b78b619
C-Red20 <caila.linger@yahoo.com>
2024-05-28 22:11:47 -0400

    Claim vs Close

--- src/components/tickets/TicketList.jsx
+++ src/components/tickets/TicketList.jsx
@@ -5,17 +5,20 @@ import { TicketFilterBar } from "./TicketFilterBar.jsx"
 import "./Tickets.css"
 
 
-export const TicketList = () => {
+export const TicketList = ({ currentUser }) => {
     const [allTickets, setAllTickets] = useState([])
     const [showEmergencyOnly, setShowEmergencyOnly] = useState(false)
     const [filteredTickets, setFilteredTickets] = useState([])
     const [searchTerm, setSearchTerm] = useState('')
-  
-    useEffect(() => {
+
+    const getAndSetTickets = () => {
       getAllTickets().then((ticketsArray) => {
         setAllTickets(ticketsArray)
-        console.log("tickets set!")
       })
+    }
+  
+    useEffect(() => {
+      getAndSetTickets()
     }, []) // ONLY runs on initial render of component
   
     useEffect(() => {
@@ -43,7 +46,14 @@ export const TicketList = () => {
         />
         <article className="tickets">
           {filteredTickets.map((ticketObj) => {
-            return <Ticket ticket={ticketObj} key={ticketObj.id}/>
+            return (
+              <Ticket 
+                ticket={ticketObj} 
+                currentUser={currentUser} 
+                getAndSetTickets={getAndSetTickets} 
+                key={ticketObj.id}
+              />
+            )
           })}
         </article>
       </div>
